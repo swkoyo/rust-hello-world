bffb94dd68c62259f4719a786304e3dda7e8492b
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-03-22 14:50:57 +0900

    temp convert

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,44 @@
-use rand::Rng;
-use std::cmp::Ordering;
+use core::panic;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    let mut temp_type = String::new();
+    let mut temp = String::new();
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Are you entering fahrenheit (f) or celcius (c)?");
+    io::stdin()
+        .read_line(&mut temp_type)
+        .expect("Failed to read line");
 
-    loop {
-        println!("Please input your guess");
+    let temp_type = temp_type.trim();
 
-        let mut guess = String::new();
+    if temp_type != "c" && temp_type != "f" {
+        panic!("Invalid type given");
+    }
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+    println!("Enter the temperature value: ");
+    io::stdin()
+        .read_line(&mut temp)
+        .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+    let temp: f64 = match temp.trim().parse() {
+        Ok(num) => {
+            if temp_type == "c" {
+                convert_c_to_f(num)
+            } else {
+                convert_f_to_c(num)
             }
         }
-    }
+        Err(_) => panic!("Enter a valid temperature"),
+    };
+
+    println!("Converted {temp_type}: {temp}");
+}
+
+fn convert_f_to_c(f_temp: f64) -> f64 {
+    (f_temp - 32.0) * (5.0 / 9.0)
+}
+
+fn convert_c_to_f(c_temp: f64) -> f64 {
+    (c_temp * (9.0 / 5.0)) + 32.0
 }

75df6cd465a97b87d7396fbf20827f2f5f3b02e9
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-03-22 15:01:28 +0900

    fib

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,27 @@
 use core::panic;
 use std::io;
 
-fn main() {
-    let mut temp_type = String::new();
-    let mut temp = String::new();
-
-    println!("Are you entering fahrenheit (f) or celcius (c)?");
-    io::stdin()
-        .read_line(&mut temp_type)
-        .expect("Failed to read line");
-
-    let temp_type = temp_type.trim();
-
-    if temp_type != "c" && temp_type != "f" {
-        panic!("Invalid type given");
+fn fib(num: u32) -> u32 {
+    if num == 0 {
+        return 0;
     }
 
-    println!("Enter the temperature value: ");
+    if num == 1 {
+        return 1;
+    }
+
+    fib(num - 1) + fib(num - 2)
+}
+
+fn main() {
+    let mut fib_n = String::new();
+    println!("Which nth Fibonacci number would you like to get?");
     io::stdin()
-        .read_line(&mut temp)
+        .read_line(&mut fib_n)
         .expect("Failed to read line");
-
-    let temp: f64 = match temp.trim().parse() {
-        Ok(num) => {
-            if temp_type == "c" {
-                convert_c_to_f(num)
-            } else {
-                convert_f_to_c(num)
-            }
-        }
-        Err(_) => panic!("Enter a valid temperature"),
+    let fib_n: u32 = match fib_n.trim().parse() {
+        Ok(num) => fib(num),
+        Err(_) => panic!("Enter a valid number"),
     };
-
-    println!("Converted {temp_type}: {temp}");
-}
-
-fn convert_f_to_c(f_temp: f64) -> f64 {
-    (f_temp - 32.0) * (5.0 / 9.0)
-}
-
-fn convert_c_to_f(c_temp: f64) -> f64 {
-    (c_temp * (9.0 / 5.0)) + 32.0
+    println!("The nth fibonacci number is: {fib_n}");
 }
